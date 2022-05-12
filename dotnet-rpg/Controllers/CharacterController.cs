dd8b83a80b1c58abbf1524751d9716fd5dba29b1
Fred Luetkemeier <fredluetkemeier@gmail.com>
2022-05-12 14:09:23 -0500

    Added action to get character by id

--- dotnet-rpg/Controllers/CharacterController.cs
+++ dotnet-rpg/Controllers/CharacterController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using dotnet_rpg.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,12 +11,25 @@ namespace dotnet_rpg.Controllers
     {
         private static List<Character> characters = new List<Character> {
             new Character(),
-            new Character { Name = "Sam" }
+            new Character { Id = 1, Name = "Sam" }
         };
 
-        [HttpGet]
+        [HttpGet("GetAll")]
         public ActionResult<Character> Get()
         {
+            return Ok(characters);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Character> GetSingle(int id)
+        {
+            return Ok(characters.FirstOrDefault(x => x.Id == id));
+        }
+
+        [HttpPost]
+        public ActionResult<List<Character>> AddCharacter(Character newCharacter)
+        {
+            characters.Add(newCharacter);
             return Ok(characters);
         }
     }
